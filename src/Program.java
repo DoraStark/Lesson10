e4a2a24a5e15b7f85e1814dc2fb1eeb997aa1538
DoraStark <156120925+DoraStark@users.noreply.github.com>
2024-02-06 01:00:22 +0100

    Update Program.java

--- src/Program.java
+++ src/Program.java
@@ -2,7 +2,7 @@
 
 public class Program {
     public static void main(String[] args) {
-        User user = new User("Alice", "qwerty");
-        user.displayAccount();
+        User alice = new User("Alice", "qwerty");
+        alice.displayAccount();
     }
 }
